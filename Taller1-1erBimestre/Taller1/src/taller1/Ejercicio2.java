a5c985dae432fce410b80d90000ed1d6150fc831
Anderson Aguirre <anderstar@gmail.com>
2018-05-17 12:30:52 -0500

    subiendo archivos clase 7 taller 1

--- Taller1-1erBimestre/Taller1/src/taller1/Ejercicio2.java
+++ Taller1-1erBimestre/Taller1/src/taller1/Ejercicio2.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  *
  * @author Anderson
  */
-public class paul {
+public class Ejercicio2 {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
